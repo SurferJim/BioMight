5ce046cd74a6234ab42ca7a441a42d4af815f5ff
James Nason <surferjm@optonline.net>
2020-02-17 09:41:59 -0500

    Add files via upload

---
+++ BioMightWeb/src/biomight/chemistry/pharma/cholinergicagonist/Pralidoxime.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Nov 10, 2006
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package biomight.chemistry.pharma.cholinergicagonist;
+
+/**
+ * @author SurferJim
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class Pralidoxime {
+
+}
---
+++ BioMightWeb/src/biomight/chemistry/pharma/opiods/Butorphanol.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Jul 22, 2007
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package biomight.chemistry.pharma.opiods;
+
+/**
+ * @author SurferJim
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class Butorphanol {
+
+}
