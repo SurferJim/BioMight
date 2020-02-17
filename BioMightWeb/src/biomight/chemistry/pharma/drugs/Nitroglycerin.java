39d52c6c2f18d5faa8e896ae79cce9ee05748574
James Nason <surferjm@optonline.net>
2020-02-17 09:43:29 -0500

    Add files via upload

---
+++ BioMightWeb/src/biomight/chemistry/pharma/drugs/Nitroglycerin.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Nov 5, 2006
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package biomight.chemistry.pharma.drugs;
+
+/**
+ * @author SurferJim
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class Nitroglycerin {
+
+}
