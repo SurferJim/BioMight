ebcd71d56f72b73fc8466f595692b784e404392c
James Nason <surferjm@optonline.net>
2020-02-17 18:21:59 -0500

    Add files via upload

---
+++ BioMightWeb/src/biomight/body/processes/Hematopoiesis.java
@@ -0,0 +1,71 @@
+/*
+ * Created on Sep 26, 2006
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package biomight.body.processes;
+import biomight.chemistry.protein.*;
+import biomight.system.tissue.connective.*;
+
+import java.math.BigDecimal;
+
+/**
+ * @author SurferJim
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class Hematopoiesis {
+	
+	private ExtraCellularMatrix extraCellularMatrix;
+	private Integrins integrins;
+	private Selectins selectins;
+	
+	
+	// Erythoid Pathway
+	
+	
+	// Myeliod Pathway
+	
+	
+	// Lymphoid Pathway
+	
+	
+	public void start()
+	{
+	}
+	
+
+	public void end()
+	{
+	}	
+	
+	// Set Stem Cell Growth Factor
+	public void setSCF(BigDecimal percentage)
+	{
+	}
+	
+	// 
+	public void setInterlukin3(BigDecimal percentage)
+	{
+	}
+	
+	public void setGMCSF(BigDecimal percentage)
+	{
+	}
+	
+	public void setGCSF(BigDecimal percentage)
+	{
+	}
+	
+	public void setIntegrins(BigDecimal percentage)
+	{
+	}
+	
+	public void setSelectins(BigDecimal percentage)
+	{
+	}
+	
+
+}
