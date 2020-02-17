d46ff0a630a93e3ea18cbceb2575eeb7cc81f854
James Nason <surferjm@optonline.net>
2020-02-17 18:01:17 -0500

    Add files via upload

---
+++ BioMightWeb/src/biomight/grafx/generateFlash.java
@@ -0,0 +1,29 @@
+/*
+ * Created on Jul 10, 2006
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package biomight.grafx;
+
+/**
+ * @author SurferJim
+ *
+ * Class which generates the FLASH SWF file based on the animation 
+ * the user constructed.
+ *
+ */
+
+public class generateFlash {
+
+
+	// Run through each sequence of the storyboard
+	// and assemble into an animation.  Each scene in the
+	// storyboard has already been validated.
+	
+	// Each Scene has been transitions through the BioMight
+	// engine to fill in animation steps.
+	
+	 
+
+}
