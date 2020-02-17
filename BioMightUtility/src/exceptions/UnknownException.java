79dc97b91a69cac6f3c5ab5aeea3ca818a31fa14
James Nason <surferjm@optonline.net>
2020-02-16 23:34:44 -0500

    Add files via upload

---
+++ BioMightUtility/src/exceptions/UnknownException.java
@@ -0,0 +1,16 @@
+package biomight.exceptions;
+
+/**
+ * @author Andrew Cohen
+ *
+ * This exception is generated when there is an error reading the 
+ * screen and tab config file (NavMenu.xml).
+ */
+public class UnknownException extends Exception {
+
+public UnknownException(String s)
+{
+	super(s);
+}
+
+}
