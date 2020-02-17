04643d8605626fcccbb87d0fb5574a5d8c182858
James Nason <surferjm@optonline.net>
2020-02-16 23:35:33 -0500

    Add files via upload

---
+++ BioMightUtility/src/util/BioDBUtils.java
@@ -0,0 +1,19 @@
+package biomight.util;
+
+
+
+/***************************************************************
+ * BIO GRAPHICS
+ * Routines for 
+ * 
+ * @param point1
+ * @param point2
+ * @return
+ ***************************************************************/
+
+
+public class BioDBUtils {
+
+
+		
+}
