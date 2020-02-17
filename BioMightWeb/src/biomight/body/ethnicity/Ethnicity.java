f17c8577238745d2a2ff4b1a9ed8e21a55118a0c
James Nason <surferjm@optonline.net>
2020-02-17 18:17:03 -0500

    Add files via upload

---
+++ BioMightWeb/src/biomight/body/ethnicity/Ethnicity.java
@@ -0,0 +1,11 @@
+package biomight.body.ethnicity;
+
+public class Ethnicity {
+	
+	private final static String Black = "black";
+	private final static String Caucasian = "caucasian";
+	private final static String Spanish = "spanish";
+	private final static String Latin = "latin";
+	private final static String Chinese = "Chinese";
+	private final static String Vietnamese = "Vietnamese";
+}
