69b4e66757ce852be3ffa7935506ce7deb2b951e
James Nason <surferjm@optonline.net>
2020-02-17 09:38:46 -0500

    Add files via upload

---
+++ BioMightWeb/src/biomight/chemistry/pharma/antagonist/Labetalo.java
@@ -0,0 +1,18 @@
+/*
+ * Created on May 21, 2007
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package biomight.chemistry.pharma.antagonist;
+import biomight.chemistry.compound.Compound;
+
+/**
+ * @author SurferJim
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class Labetalo extends Compound {
+
+}
---
+++ BioMightWeb/src/biomight/chemistry/pharma/antimicrobial/Clofazimine.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Jul 22, 2007
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package biomight.chemistry.pharma.antimicrobial;
+import biomight.chemistry.compound.Compound;
+
+/**
+ * @author SurferJim
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class Clofazimine extends Compound {
+
+}
