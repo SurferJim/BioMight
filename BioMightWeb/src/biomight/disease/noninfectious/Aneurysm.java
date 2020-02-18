3b776be6ed10ebfe57bbdfe22064c2e1fa36d250
James Nason <surferjm@optonline.net>
2020-02-17 20:47:38 -0500

    Add files via upload

---
+++ BioMightWeb/src/biomight/disease/noninfectious/Aneurysm.java
@@ -0,0 +1,24 @@
+/*
+ * Created on May 10, 2006
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package biomight.disease.noninfectious;
+import biomight.BioMightBase;
+import java.math.BigDecimal;
+
+
+/**
+ * @author SurferJim
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+
+public class Aneurysm extends BioMightBase {
+
+
+	
+	
+}
