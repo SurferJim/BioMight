db0003f6ec1fdbf9598b918156f1b2f83c4d7a25
James Nason <surferjm@optonline.net>
2020-02-17 20:29:34 -0500

    Add files via upload

---
+++ BioMightWeb/src/biomight/system/myology/fasciae/SuperficialFascia.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Jul 6, 2006
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package biomight.system.myology.fasciae;
+
+/**
+ * @author SurferJim
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class SuperficialFascia extends Fascia {
+
+}
