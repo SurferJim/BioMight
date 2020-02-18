ca31e846b57d9d365fce0452ed6fe73f36c4ef19
James Nason <surferjm@optonline.net>
2020-02-17 20:39:57 -0500

    Add files via upload

---
+++ BioMightWeb/src/biomight/cascades/Cascades.java
@@ -0,0 +1,24 @@
+/*
+ * Created on Feb 10, 2007
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package biomight.cascades;
+
+import biomight.BioMightBase;
+
+/**
+ * @author SurferJim
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class Cascades extends BioMightBase {
+	
+	public Cascades()
+	{
+		this.setImage("images/Cascades.jpg");
+	}
+
+}
